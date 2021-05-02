9c1957298c5d0c48ca74161718bfe1797eb1e30b
ismael45 <61148057+ismael45@users.noreply.github.com>
2021-05-02 13:15:44 +0200

    Update Mando_a_distancia (borrador).ino

--- Mando_a_distancia (borrador).ino
+++ Mando_a_distancia (borrador).ino
@@ -1,9 +1,9 @@
-#include <IRremote.h>
+#include <IRremote.h>//Incluir biblioteca del fabricante (irremote de shirriff version 3.3.0)
 #define Boton_1 0xFF30CF
 #define Boton_2 0xFF18E7
-#define Boton_3 0xFF7A85
+#define Boton_3 0xFF7A85//Asignar al codigo de cada boton del mando una variable
 
-int SENSOR = 11;
+int SENSOR = 11; //Ejemplo enencdido-apagado tres led (rojo,verde,amarillo)
 IRrecv irrecv(SENSOR);
 decode_results codigo;
 int LEDR = 4;
