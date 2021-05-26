b17ca754beb75b1ae36fb4470f82cc3fd23b66b9
JaviNico <61287460+JaviNico@users.noreply.github.com>
2021-05-26 19:40:03 +0200

    Add files via upload

---
+++ ConexionSerieWired.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SerialClass/SerialClass.h"
+
+int main()
+{
+    Serial* Arduino; 
+
+    char puerto[]="COM3"; // depende del puerto
+    int cancion = 0;
+    char secuencia[2];
+
+    Arduino = new Serial((char *)puerto);
+    // Colocar esta seccion en el lugar donde se quierre que se escuche la canción 
+    if (Arduino->IsConnected())
+    {
+
+        cancion = 1; 
+
+        sprintf_s(secuencia, "%d", cancion);
+
+        Arduino->WriteData(secuencia, strlen(secuencia));
+        Sleep(2000);
+    }
+
+    // Colocar esta seccion en el lugar donde se quierre que se escuche la canción 
+    if (Arduino->IsConnected())
+    {
+
+        cancion = 2; 
+
+        sprintf_s(secuencia, "%d", cancion);
+
+        Arduino->WriteData(secuencia, strlen(secuencia));
+        Sleep(2000);
+
+    }
+    
+}
+
